81a31715789a84414a26134c4a60393e66059fb4
Nehan Yaser <67751528+webdevsint@users.noreply.github.com>
2023-06-09 00:31:11 +0600

    Backup validation

--- scripts/backup.js
+++ scripts/backup.js
@@ -27,17 +27,31 @@ function readJSON(file) {
     const contents = event.target.result;
     const jsonData = JSON.parse(contents);
 
-    db.clear();
+    if (
+      (jsonData[0].id &&
+        jsonData[0].mood &&
+        jsonData[0].log &&
+        jsonData[0].date &&
+        jsonData[jsonData.length - 1].id &&
+        jsonData[jsonData.length - 1].mood &&
+        jsonData[jsonData.length - 1].log &&
+        jsonData[jsonData.length - 1].date) ||
+      jsonData === []
+    ) {
+      db.clear();
 
-    jsonData.forEach((item) => {
-      db.write(item.id, {
-        mood: item.mood,
-        log: item.log,
-        date: item.date,
+      jsonData.forEach((item) => {
+        db.write(item.id, {
+          mood: item.mood,
+          log: item.log,
+          date: item.date,
+        });
       });
-    });
 
-    location.replace("/logs.html");
+      location.replace("/logs.html");
+    } else {
+      alert("No backup found!");
+    }
   };
 
   reader.readAsText(file);
