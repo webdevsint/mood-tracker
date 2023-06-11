6a73c0a835dd387f180a34c7ba762e0160aba720
Nehan Yaser <67751528+webdevsint@users.noreply.github.com>
2023-06-11 17:45:34 +0600

    PWA update support

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,33 +1,47 @@
-const cacheName = "moodtracker";
+const cacheName = "moodtracker-1.2";
 
+const assets = [
+  "./",
+  "./index.html",
+  "./logs.html",
+  "./backup.html",
+  "./styles/index.css",
+  "./styles/logs.css",
+  "./styles/backup.css",
+  "./scripts/index.js",
+  "./scripts/logs.js",
+  "./scripts/backup.js",
+  "./scripts/localdb.js",
+  "./manifest.json",
+];
+
+// install event
 self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open(cacheName).then((cache) => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./logs.html",
-        "./backup.html",
-        "./styles/index.css",
-        "./styles/logs.css",
-        "./styles/backup.css",
-        "./scripts/index.js",
-        "./scripts/logs.js",
-        "./scripts/backup.js",
-        "./scripts/localdb.js",
-        "./manifest.json",
-      ]);
+      cache.addAll(assets);
     })
   );
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches
-      .open(cacheName)
-      .then((cache) => cache.match(event.request, { ignoreSearch: true }))
-      .then((response) => {
-        return response || fetch(event.request);
-      })
+// activate event
+self.addEventListener("activate", (e) => {
+  self.skipWaiting();
+
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== cacheName).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// fetch event
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cacheRes) => {
+      return cacheRes || fetch(e.request);
+    })
   );
 });
